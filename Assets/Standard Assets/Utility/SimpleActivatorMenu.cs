6c8d1ff0314519a5ef7595a352386f065910b36d
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-10-13 22:31:49 +0200

    fix: Solve Assets\Utility\SimpleActivatorMenu.cs: error CS0619: 'GUIText' is obsolete: 'GUIText has been removed. Use UI.Text instead.'

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI; //Jaiden addition
 
 #pragma warning disable 618
 namespace UnityStandardAssets.Utility
@@ -8,7 +9,8 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        // public GUIText camSwitchButton;
+        public Text camSwitchButton;  //Jaiden addition
         public GameObject[] objects;
 
 
