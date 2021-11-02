ae999e74a69f2e0e264c41bc9ee2ffc29d085abd
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-02 10:21:11 +0100

    feat: Adjust change of material

--- Assets/Scripts/PaintInBlack.cs
+++ Assets/Scripts/PaintInBlack.cs
@@ -8,6 +8,19 @@ public class PaintInBlack : MonoBehaviour
 
     void start() {
         print(House.transform.childCount);
+
+        Transform[] allChildren = House.GetComponentsInChildren<Transform>();
+        List<GameObject> childObjects = new List<GameObject>();
+        foreach (Transform child in allChildren)
+        { 
+            childObjects.Add(child.gameObject);
+        }
+        
+        /*MeshRenderer my_renderer = House.GetComponent<MeshRenderer>();
+        if ( my_renderer != null )
+        {
+            my_renderer.material = ;
+        }*/
     }
     void Update()
     {
