49e77abfba236d08b3d2e385830840e7600ef093
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-02 03:50:52 +0100

    feat: Create paint in black script

---
+++ Assets/Scripts/PaintInBlack.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintInBlack : MonoBehaviour
+{
+    public GameObject House;
+
+    void start() {
+        print(House.transform.childCount);
+    }
+    void Update()
+    {
+        
+    }
+}
